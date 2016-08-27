46a274ad266135e23e01a7d7aca9ffcf02260066
AlexWang <1669499355@qq.com>
2016-08-27 09:54:01 +0800

    local_dev_commit

--- etc/webpack.features.js
+++ etc/webpack.features.js
@@ -4,16 +4,24 @@ var HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = function (cx, umdConf) {
   return {
-    enableCleanFeature: function () {
-      umdConf.addPlugin(new CleanWebpackPlugin([cx.__builddir], {
+    enableClean: function (absPaths) {
+      var mergeAbsPaths = absPaths || [cx.__builddir];
+      umdConf.addPlugin(new CleanWebpackPlugin(mergeAbsPaths, {
         root: cx.__cwd
       }))
     },
-    enableEntryHTML: function (file_name) {
-      umdConf.addPlugin(new HTMLWebpackPlugin({
-        filename: file_name || 'index.html',
+    enableEntryHTML: function (options) {
+      var mergeOptions = Object.assign({}, {
+        filename: 'index.html',
         template: cx.getCwdPath('./etc/umd.template.html')
-      }));
+      }, options);
+      umdConf.addPlugin(new HTMLWebpackPlugin(mergeOptions));
+    },
+    enableEntryHot: function (entryName) {
+      var webpackHotClient = require.resolve('webpack-hot-middleware/client') + '?reload=true';
+      var entryBundle = this.entry[entryName || 'main'];
+      if (entryBundle)
+        entryBundle.unshift(webpackHotClient);
     }
   }
 };
